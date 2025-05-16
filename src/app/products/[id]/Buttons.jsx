558435386eb53c2a1e0696fb0e63674ee0b65e4d
Marcell Alessandro Reynoso Flores <marcell.reynoso.flores@gmail.com>
2025-05-16 15:45:20 -0500

    Funcionalidad de borrar un producto

---
+++ src/app/products/[id]/Buttons.jsx
@@ -0,0 +1,40 @@
+"use client";
+
+import axios from "axios";
+import { useRouter } from "next/navigation";
+
+function Buttons({ productId }) {
+  const router = useRouter();
+  return (
+    <div className="flex gap-x-2 mt-4">
+      <button
+        className="
+            bg-yellow-500 text-white
+            hover:bg-yellow-700 hover:font-semibold hover:cursor-pointer
+            py-3 px-3 rounded-lg"
+      >
+        Edit
+      </button>
+      <button
+        className="
+          bg-red-500 text-white 
+          hover:bg-red-700 hover:font-semibold hover:cursor-pointer
+            py-3 px-3 rounded-lg"
+        onClick={async () => {
+          if (confirm("¿Estás segura que quieres borrar el producto?")) {
+            const res = await axios.delete("/api/products/" + productId);
+            console.log(`Borrando producto: ${productId}`);
+            if (res.status === 204) {
+              router.push("/products");
+              router.refresh();
+            }
+          }
+        }}
+      >
+        Delete
+      </button>
+    </div>
+  );
+}
+
+export default Buttons;
--- src/app/products/[id]/page.jsx
+++ src/app/products/[id]/page.jsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import Buttons from "./Buttons";
 
 async function loadSingleProduct(productId) {
   const singleProduct = await axios.get(
@@ -9,36 +10,25 @@ async function loadSingleProduct(productId) {
 
 async function page({ params }) {
   let productParams = await params;
-  let singleProduct = await loadSingleProduct(productParams.id);
+  let productArray = await loadSingleProduct(productParams.id);
+  let product = productArray.data[0];
   return (
     <section className="flex justify-center items-center ">
       <div className="p-6 bg-gray-50 rounded-lg">
-        <h1 className="text-lg font-bold">{singleProduct.data[0].name}</h1>
+        <h1 className="text-lg font-bold">{product.name}</h1>
         <h2 className="text-2xl text-slate-600">
-          {singleProduct.data[0].price}
+          {product.price}
         </h2>
-        <p>{singleProduct.data[0].description}</p>
-        <div className="flex gap-x-2 mt-4">
-          <button
-            className="
-        bg-yellow-500 text-white
-        hover:bg-yellow-700 hover:font-semibold hover:cursor-pointer
-        py-3 px-3 rounded-lg"
-          >
-            Edit
-          </button>
-          <button
-            className="
-        bg-red-500 text-white 
-        hover:bg-red-700 hover:font-semibold hover:cursor-pointer
-        py-3 px-3 rounded-lg"
-          >
-            Delete
-          </button>
-        </div>
+        <p>{product.description}</p>
+        <Buttons productId = {product.id} />
       </div>
     </section>
   );
 }
 
 export default page;
+
+
+// TypeError: Cannot read properties of undefined (reading 'name')
+//     at Object.loadSingleProduct (page.jsx:8:1)
+
